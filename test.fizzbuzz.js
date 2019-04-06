f708ab8d0de038eb610f6fd07b88271b43dd760d
chetta chetta chetta <>
2019-04-06 10:47:06 +0700

    verify case 3

--- test.fizzbuzz.js
+++ test.fizzbuzz.js
@@ -1,4 +1,7 @@
 function fizzbuzz(num) {
+  if(num == 3) {
+    return "Fizz"
+  }
   return num
 }
 
@@ -8,5 +11,8 @@ describe('Fizzbuzz', function() {
   })
   it('should return 2 when the value is 2', () => {
      chai.assert.equal(2, fizzbuzz(2))
+  })
+  it('should return Fizz when the value is 3', () => {
+    chai.assert.equal("Fizz", fizzbuzz(3))
   })
 })

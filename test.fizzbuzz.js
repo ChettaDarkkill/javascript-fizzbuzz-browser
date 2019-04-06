664a2d307bbc9cb8ccec7c3e7a16e69e76f58fc6
chetta chetta chetta <>
2019-04-06 11:24:04 +0700

    move js file

--- test.fizzbuzz.js
+++ test.fizzbuzz.js
@@ -1,16 +1,3 @@
-function fizzbuzz(num) {
-  if(num % 15 == 0) {
-    return "FizzBuzz"
-  }
-  if(num % 3 == 0) {
-    return "Fizz"
-  }
-  if(num % 5 == 0) {
-    return "Buzz"
-  }
-  return num
-}
-
 describe('Fizzbuzz', function() {
   it('should return 1 when the value is 1', () => {
      chai.assert.equal(1, fizzbuzz(1))
